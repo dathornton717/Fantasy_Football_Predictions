d30cdb4209ade7d63c9b0eba5a626ed0fa7b4ea6
David Thornton <thornton.da@husky.neu.edu>
2018-07-10 21:49:21 -0700

    OldOffenseCompositeKey comments

--- Server/src/main/java/fantasy/predictor/entity/keys/OldOffenseCompositeKey.java
+++ Server/src/main/java/fantasy/predictor/entity/keys/OldOffenseCompositeKey.java
@@ -3,11 +3,18 @@ package fantasy.predictor.entity.keys;
 import java.io.Serializable;
 import java.util.Objects;
 
+// Represents a database key for an old offense stat
 public class OldOffenseCompositeKey implements Serializable {
   private String name;
   private String team;
   private String position;
 
+  /**
+   * Does the given Object equal this OldOffenseCompositeKey?
+   * @param o The Object to compare to
+   * @return If the given Object has the same name, team, and position as this
+   *        OldOffenseCompositeKey
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -22,6 +29,10 @@ public class OldOffenseCompositeKey implements Serializable {
     return name.equals(that.name) && team.equals(that.team) && position.equals(that.position);
   }
 
+  /**
+   * Generate a hash value for this OldOffenseCompositeKey.
+   * @return The int hash for this OldOffenseCompositeKey
+   */
   @Override
   public int hashCode() {
     return Objects.hash(name, team, position);
