b43dd0ccccfc849b4b0dc7496ba89aa9afc6cf51
David Thornton <thornton.da@husky.neu.edu>
2018-07-10 21:40:31 -0700

    Site comments

--- Server/src/main/java/fantasy/predictor/entity/enums/Site.java
+++ Server/src/main/java/fantasy/predictor/entity/enums/Site.java
@@ -1,5 +1,6 @@
 package fantasy.predictor.entity.enums;
 
+// Represents a site that predicts fantasy points
 public enum Site {
   NFL("NFL.com"), CBS("cbssports.com"), ESPN("ESPN.com");
 
@@ -9,10 +10,19 @@ public enum Site {
     this.value = value;
   }
 
+  /**
+   * Get the String value of the site.
+   * @return The value of the site
+   */
   public String getValue() {
     return value;
   }
 
+  /**
+   * Parse the given String into a site or return null if it is not a valid String.
+   * @param s The String to check
+   * @return The site or null if illegal
+   */
   public static Site parse(String s) {
     for (Site site : Site.values()) {
       if (site.getValue().equals(s)) {
