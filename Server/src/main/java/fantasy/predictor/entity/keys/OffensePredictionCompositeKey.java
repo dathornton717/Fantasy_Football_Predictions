75f307826567a36553a80df3692ebb63db1c5584
David Thornton <thornton.da@husky.neu.edu>
2018-07-10 21:46:26 -0700

    OffensePredictionCompositeKey comments

--- Server/src/main/java/fantasy/predictor/entity/keys/OffensePredictionCompositeKey.java
+++ Server/src/main/java/fantasy/predictor/entity/keys/OffensePredictionCompositeKey.java
@@ -3,12 +3,19 @@ package fantasy.predictor.entity.keys;
 import java.io.Serializable;
 import java.util.Objects;
 
+// Represents a database composite key for an Offense Prediction
 public class OffensePredictionCompositeKey implements Serializable {
   private String name;
   private String team;
   private String position;
   private String site;
 
+  /**
+   * Does this OffensePredictionCompositeKey equal the given Object?
+   * @param o The Object to compare this OffensePredictionCompositeKey to
+   * @return If the given Object has the same name, team, position, and site as this
+   *        OffensePredictionCompositeKey
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -24,6 +31,10 @@ public class OffensePredictionCompositeKey implements Serializable {
             && site.equals(that.site);
   }
 
+  /**
+   * Generate a hash value for this OffensePredictionCompositeKey.
+   * @return The int hash for this OffensePredictionCompositeKey
+   */
   @Override
   public int hashCode() {
     return Objects.hash(name, team, position, site);
