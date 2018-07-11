42db1ac0307490e9b454a26c6a91396de27f34ad
David Thornton <thornton.da@husky.neu.edu>
2018-07-10 21:47:49 -0700

    OldKickerCompositeKey comments

--- Server/src/main/java/fantasy/predictor/entity/keys/OldKickerCompositeKey.java
+++ Server/src/main/java/fantasy/predictor/entity/keys/OldKickerCompositeKey.java
@@ -3,10 +3,16 @@ package fantasy.predictor.entity.keys;
 import java.io.Serializable;
 import java.util.Objects;
 
+// Represents a database key for an old kicker
 public class OldKickerCompositeKey implements Serializable {
   private String name;
   private String team;
 
+  /**
+   * Does this OldKickerCompositeKey equal the given Object?
+   * @param o The Object to compare to
+   * @return If the given Object has the same name and team as this OldKickerCompositeKey
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -21,6 +27,10 @@ public class OldKickerCompositeKey implements Serializable {
     return name.equals(that.name) && team.equals(that.team);
   }
 
+  /**
+   * Generate a hash value for this OldKickerCompositeKey
+   * @return The int hash for this OldKickerCompositeKey
+   */
   @Override
   public int hashCode() {
     return Objects.hash(name, team);
