90857531be57c0eecec4600b9c5df567d891cce4
David Thornton <thornton.da@husky.neu.edu>
2018-07-10 21:42:20 -0700

    DefensePredictionCompositeKey comments

--- Server/src/main/java/fantasy/predictor/entity/keys/DefensePredictionCompositeKey.java
+++ Server/src/main/java/fantasy/predictor/entity/keys/DefensePredictionCompositeKey.java
@@ -3,10 +3,16 @@ package fantasy.predictor.entity.keys;
 import java.io.Serializable;
 import java.util.Objects;
 
+// Represents a composite key in the database for a defense prediction
 public class DefensePredictionCompositeKey implements Serializable {
   private String name;
   private String site;
 
+  /**
+   * Does this DefensePredictionCompositeKey equal the given Object?
+   * @param o The given object
+   * @return If the objects have the same name and site.
+   */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -21,6 +27,10 @@ public class DefensePredictionCompositeKey implements Serializable {
     return name.equals(that.name) && site.equals(that.site);
   }
 
+  /**
+   * Generates a hash of this DefensePredictionCompositeKey.
+   * @return The int hash of this DefensePredictionCompositeKey
+   */
   @Override
   public int hashCode() {
     return Objects.hash(name, site);
